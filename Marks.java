3b65853b5af104ea7e2eaffc511f1f9b79603cf2
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2023-08-08 00:07:07 +0530

    Initial Commit

---
+++ Marks.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Marks{
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter your Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your Subject 1: ");
+        String sub1 = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.print("Enter your Mark 1: ");
+        double mark1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Enter your Subject 2: ");
+        String sub2 = scanner.nextLine();
+
+        System.out.print("Enter your Mark 2: ");
+        double mark2= scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Enter your Subject 3: ");
+        String sub3 = scanner.nextLine();
+
+        System.out.print("Enter your Mark 3: ");
+        double mark3 = scanner.nextDouble();
+        scanner.nextLine();
+
+        //check name input
+        
+
+
+    }
+}
